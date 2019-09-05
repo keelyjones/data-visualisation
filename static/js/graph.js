9c4576a4a6b7fb88d7cdac92a4f3793e8fa7285f
Ubuntu <ubuntu@ip-172-31-44-86.ec2.internal>
2019-09-05 11:19:06 +0000

    fixed JS errors, completed line graph

--- static/js/graph.js
+++ static/js/graph.js
@@ -12,7 +12,7 @@ function makeGraphs(error, employmentData) {
 
 function show_wage_gap(ndx) {
     var dim = ndx.dimension(dc.pluck('year'));
-    var group = dim.group();
+    var group = dim.group().reduceSum(dc.pluck("gender.wage.gap"))
     
     dc.lineChart("#wage-gap")
         .width(1300)
@@ -28,18 +28,3 @@ function show_wage_gap(ndx) {
         .yAxis().ticks(10);
 }
 
-
-var timeDimension = cf.dimension(function(d){ return new Date(d.Time); });
-var totalGroup = timeDimension.group().reduceSum(function(d){ return d.Speed; });
-
-var lineChart = dc.lineChart("#line-chart")
-    .brushOn(false)
-    .width(800)
-    .height(200)
-    .x(d3.time.scale().domain(d3.extent(data, function(d) {
-        return new Date(d.Time);
-    })))
-    .dimension(timeDimension)
-    .group(totalGroup);
-
-dc.renderAll();
